1d6f3d34af75606364826b9a40d9e2d822c15af7
takkyuuplayer <takkyuuplayer@gmail.com>
2021-01-02 16:22:05 +0900

    Fix

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,7 +1,10 @@
-import { TRAFIC_LIGHT } from "../src/sample";
+import { TRAFIC_LIGHT, add } from "../src/sample";
 describe("sample", () => {
   it("should pass", () => {
     expect(1).toBe(1);
+  });
+  it("should add", () => {
+    expect(add(2, 3)).toBe(5);
   });
   describe("TRAFIC_LIGHT", () => {
     it("has 6 keys including reverse map", () => {
